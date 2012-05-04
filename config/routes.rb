65655a48da07d2b271cfb7561d8a46b5dba8fa21
carl sednaoui <carlsednaoui@gmail.com>
2012-05-04 00:21:38 -0400

    foo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Networkmill::Application.routes.draw do
   root :to => "users#index"
 
+  resources :identities
   resources :users
   resources :contacts
-  resources :identities
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+  match "/auth/failure" => "sessions#failure"
 end
