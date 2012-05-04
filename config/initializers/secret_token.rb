b503cc0e75cf243784fee1704373280dbf04f15f
carl sednaoui <carlsednaoui@gmail.com>
2012-05-03 23:02:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Networkmill::Application.config.secret_token = 'fc475f23a0642e06a68f1882f834acf2382929e23412ce99e33faeb18f36e8c005219d334603e594b12b89c8b97aafab6750c3490d4b915d6c3fbdd37979e934'
