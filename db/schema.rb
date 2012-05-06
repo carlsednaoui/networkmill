722e968a64c5bbd5c242ce4a9224d297b88aeee6
carl sednaoui <carlsednaoui@gmail.com>
2012-05-06 18:03:48 -0400

    added in rotation to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505161420) do
+ActiveRecord::Schema.define(:version => 20120506220207) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120505161420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.boolean  "in_rotation", :default => true
   end
 
   create_table "identities", :force => true do |t|
