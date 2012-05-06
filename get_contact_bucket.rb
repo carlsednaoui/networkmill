fe1a43ce33210f79ccc96b071c789c714acb3f6f
carl sednaoui <carlsednaoui@gmail.com>
2012-05-06 12:07:57 -0400

    working on get_contact_bucket

--- get_contact_bucket.rb
+++ get_contact_bucket.rb
@@ -1,21 +1,23 @@
 user = User.find_by_id(1)
 
-@contact_bucket = Array.new
 def create_contact_bucket(user_id)
-   contacts = Contact.find_all_by_user_id(user_id)
-   contacts.each do |contact|
-	@contact_bucket.push contact.id
-   end
+  @contact_bucket = Array.new
+  contacts = Contact.find_all_by_user_id(user_id)
+  contacts.each do |contact|
+    @contact_bucket.push contact.id
+  end
 end
 
 def get_contacts_from_contact_bucket(contact_bucket, contact_intensity)
-  @contact_bucket = contact_bucket
-  puts "contacts to email:"
-  to_email = @contact_bucket.first(contact_intensity)
-  puts to_email
-  @contact_bucket.shift(contact_intensity)
+  send_email(contact_bucket.first(contact_intensity))
+  contact_bucket.shift(contact_intensity)
   puts "contacts left:"
-  puts @contact_bucket.inspect
+  puts contact_bucket.inspect
+end
+
+def send_email(contacts_to_email)
+  puts "Hey you, you should email these contacts:"
+  puts contacts_to_email
 end
 
 
