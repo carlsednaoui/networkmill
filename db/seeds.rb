e355e6527ab5b565908e258fa7a7714b536f6991
carl sednaoui <carlsednaoui@gmail.com>
2012-05-06 12:47:54 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Contact.create(:name => "seeb contact 1", :email => "seed1@mail.com", :user_id => User.first.id)
+Contact.create(:name => "seeb contact 2", :email => "seed2@mail.com", :user_id => User.first.id)
+Contact.create(:name => "seeb contact 3", :email => "seed3@mail.com", :user_id => User.first.id)
+Contact.create(:name => "seeb contact 4", :email => "seed4@mail.com", :user_id => User.first.id)
+Contact.create(:name => "seeb contact 5", :email => "seed5@mail.com", :user_id => User.first.id)
+Contact.create(:name => "seeb contact 6", :email => "seed6@mail.com", :user_id => User.first.id)
