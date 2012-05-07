51378355c7966506a7450a6e6b5d6d3f67f5a342
carl sednaoui <carlsednaoui@gmail.com>
2012-05-07 18:59:57 -0400

    fixed seed so that each contact has a unique name and unique email

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 User.create(:name => "seed user", :email => "seed@user.com", :password => "password")
 @user = User.find_by_email("seed@user.com")
-Contact.create(:name => "seed contact 1", :email => "seed1@mail.com", :user_id => @user.id, :state => :in)
-Contact.create(:name => "seed contact 2", :email => "seed2@mail.com", :user_id => @user.id, :state => :in)
-Contact.create(:name => "seed contact 3", :email => "seed3@mail.com", :user_id => @user.id, :state => :in)
-Contact.create(:name => "seed contact 4", :email => "seed4@mail.com", :user_id => @user.id, :state => :in)
-Contact.create(:name => "seed contact 5", :email => "seed5@mail.com", :user_id => @user.id, :state => :in)
-Contact.create(:name => "seed contact 6", :email => "seed6@mail.com", :user_id => @user.id, :state => :in)
+
+
+for c in (1..10)
+  @contacts = @user.contacts.count + 1
+  Contact.create(:name => "seed contact " + @contacts.to_s, :email => "seed" + @contacts.to_s + "@mail.com", :user_id => @user.id, :state => :in)
+end
+
 puts "---------------------------------------------------------------------"
 puts "database seeded - login for example user is seed@user.com // password"
+puts "database seeded - created " + c.to_s + " contacts for example user" 
 puts "---------------------------------------------------------------------"
