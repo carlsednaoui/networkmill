cd4beac27078aec7251c9ccfeab053008cbab4ed
carl sednaoui <carlsednaoui@gmail.com>
2012-05-07 19:33:08 -0400

    added contact & user validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ActiveRecord::Base
 
   validates_presence_of :email
   validates_format_of :email, :with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+  validates_uniqueness_of :user && :email
 
   scope :in_rotation, where(:state => :in)
   scope :last_week, where(:state => :just_used)
