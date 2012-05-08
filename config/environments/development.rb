b550c12a6518f481c33dbca032991427d3df9d54
carl sednaoui <carlsednaoui@gmail.com>
2012-05-07 23:12:16 -0400

    added mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,22 @@ Networkmill::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "networkmill.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "networkmill@gmail.com",
+    password: "ilovetonetwork"
+  }
+
+  # Specify what domain to use for mailer URLs
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
