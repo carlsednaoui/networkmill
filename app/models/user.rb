0db83c904638d5e8b2debb221c9054c7e5b03fd0
carl sednaoui <carlsednaoui@gmail.com>
2012-05-07 23:14:45 -0400

    commented welcome email out - let's not spam the world as we test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :contacts
   attr_accessible :name, :email, :desktop_client, :contact_intensity, :password, :remember_me
   before_create :default_values
-  after_create :send_welcome_mail
+ # after_create :send_welcome_mail
 
   def send_welcome_mail
      UserMailer.welcome_email(self.email).deliver
