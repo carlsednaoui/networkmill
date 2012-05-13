287fa182b2c4db5f360dab83242847c8f5a9714f
Jeff Escalante <hello@jenius.me>
2012-05-13 19:02:32 -0400

    fixed sign in css bug, z-index bug, and signed in users now always redirect to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => :dashboard
   def index
+    redirect_to dashboard_path if current_user
   end
 
   def dashboard
