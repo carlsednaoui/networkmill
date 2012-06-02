a9b13e6aff99759c5a067ff854881b0b2a7887d9
Jeff Escalante <hello@jenius.me>
2012-06-02 17:44:28 -0400

    ajax updates to user preferences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,10 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      # Sign in the user bypassing validation in case his password changed
-      sign_in @user, :bypass => true
-      redirect_to dashboard_path, :notice => 'Your settings were successfully updated.'
+      @success = true
+      # sign_in @user, :bypass => true
     else
-      render "edit", :notice => 'Saved failed'
+      # render "edit", :notice => 'Saved failed'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :contacts
   has_many :emails
   validates :contact_intensity, :numericality => { :only_integer => true, :less_than_or_equal_to => :contact_validation}, :on => :update
-  attr_accessible :name, :email, :unsubscribed, :desktop_client, :network_mode, :contact_intensity, :password, :remember_me
+  attr_accessible :name, :email, :unsubscribed, :desktop_client, :network_mode, :contact_intensity, :password, :remember_me, :signature
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
