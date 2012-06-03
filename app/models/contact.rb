060ecbaf519a103211d984da4b06006dcfc7f710
Jeff Escalante <hello@jenius.me>
2012-06-03 16:37:04 -0400

    added carrierwave

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ActiveRecord::Base
   belongs_to :user
   belongs_to :event_queue
   attr_accessible :name, :email, :promote_networkmill, :frequency, :state, :user_id, :note
+  mount_uploader :avatar, AvatarUploader
 
   validates_presence_of :email
   validates_format_of :email, :with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
--- db/migrate/20120504030701_create_contacts.rb
+++ db/migrate/20120504030701_create_contacts.rb
@@ -6,6 +6,7 @@ class CreateContacts < ActiveRecord::Migration
       t.string :email
       t.text :note
       t.string :state
+      t.string :avatar
       t.boolean :promote_networkmill
       t.integer :event_queue_id
 
