028174ee3863f1fde4bd069e9ace41d586878856
Jeff Escalante <hello@jenius.me>
2012-06-09 15:03:17 -0400

    contact added via html, validated with javascript

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,10 +21,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.user = current_user
     @contact.state = "in"
-    # if @contact.promote_networkmill == true
-    #   UserMailer.user_referral_via_new_contact(current_user, @contact).deliver
-    # end    
-    @success = true if @contact.save
+    if @contact.save
+      redirect_to dashboard_path, :notice => "contact saved!"
+    else
+      render 'edit'
+    end
   end
 
   def update
@@ -33,7 +34,7 @@ class ContactsController < ApplicationController
     if @contact.update_attributes(params[:contact])
       redirect_to contacts_url, :notice => 'Contact was successfully updated.'
     else
-      render action: "edit"
+      render :action => "edit"
     end
   end
 
