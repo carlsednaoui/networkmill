3a3ff3785154e577f352eef57064106a36f2051e
carl sednaoui <carlsednaoui@gmail.com>
2012-07-16 23:27:27 -0400

    fixed a tiny bug in our pick_random_contact and added comments to models and cleaned up some old code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,11 +8,4 @@ class Contact < ActiveRecord::Base
   validates_format_of :email, :with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
   validates_uniqueness_of :email, :scope => :user_id, :message => "already exists as a contact."
 
-  # Not sure we need to use scopes for this
-  #  scope :in_rotation, where (:state => :in)
-  #  scope :last_week, where(:state => :just_used)
-
-  def in_rotation?
-    state == "in" ? true : false
-  end
 end
