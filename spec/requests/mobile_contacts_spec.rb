87b10b9f5d64d743974c49fa776d7dd954db6633
carl sednaoui <carlsednaoui@gmail.com>
2012-08-29 21:55:40 -0400

    add mobile contact spec

---
+++ spec/requests/mobile_contacts_spec.rb
@@ -0,0 +1,23 @@
+# require 'spec_helper'
+
+# describe "Mobile Users" do
+#   include Helpers
+
+#   describe "Test log_mobile_user_in" do
+#     it "works. Now it's time to write some real shiats" do
+#       log_mobile_user_in
+#     end
+#   end
+
+#   describe "add a contact" do
+#     it "adds a contact on your mobile phone" do
+#       log_mobile_user_in
+
+#       fill_in "name", :with => "test contact"
+#       fill_in "email", :with => "testcontact@example.com"
+#       click_button "send"
+
+#       page.should have_content "contact saved!"
+#     end
+#   end
+# end
