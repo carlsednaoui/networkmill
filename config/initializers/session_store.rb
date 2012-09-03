e646648f848808b98ca726c564bd183e14199a80
Jeff Escalante <hello@jenius.me>
2012-09-03 19:40:37 -0400

    extend session lifetime for mobile web app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Networkmill::Application.config.session_store :cookie_store, key: '_networkmill_session'
+Networkmill::Application.config.session_store :cookie_store, key: '_networkmill_session', expire_after: 1.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
