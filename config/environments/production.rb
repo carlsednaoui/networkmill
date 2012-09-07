b3395d2866e91f36936f161f36ea38f1f11747c4
carl sednaoui <carlsednaoui@gmail.com>
2012-09-06 23:59:11 -0400

    test subdomain stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Networkmill::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_dispatch.tld_length = 2
 
   # Code is not reloaded between requests
   config.cache_classes = true
